5af4534105f86abd0c394346141702de5430a456
kumusan <ryogi0200@gmail.com>
2020-01-10 13:21:54 +0900

    config

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
+}
+
+ReactDOM.render( <App /> , document.getElementById('app'))
